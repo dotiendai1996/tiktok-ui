a0d144855f4649287e663fc9cbaa0d35a20d693b
Do Tien Dai <dai.do@apac.merkleinc.com>
2022-05-17 23:03:17 +0700

    Style logo and search field #1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,4 +1,7 @@
 import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import images from "~/assets/images";
 import styles from "./Header.module.scss";
 
 const cx = classNames.bind(styles);
@@ -6,7 +9,20 @@ const cx = classNames.bind(styles);
 function Header() {
   return (
     <header className={cx("wrapper")}>
-      <div className={cx("container")}></div>
+      <div className={cx("container")}>
+        <img src={images.logo} alt="Tiktok logo" />
+        <div className={cx("search")}>
+          <input placeholder="Search accounts and videos" spellCheck={false} />
+          <button className={cx("clear")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+          <button className={cx("search-btn")}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx("action")}></div>
+      </div>
     </header>
   );
 }
