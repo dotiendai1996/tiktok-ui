976fccfa1cc0068d3148a427df558b3cd997e90d
Do Tien Dai <dai.do@apac.merkleinc.com>
2022-05-17 10:55:03 +0700

    Prettier setting format code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from './components/Button';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/Button";
 
 function App() {
   return (
@@ -8,12 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Button />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
