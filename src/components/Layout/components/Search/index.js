e5c1d6c85bd028d15a0c9f0bc939a78d92a95722
Do Tien Dai <dai.do@apac.merkleinc.com>
2022-05-26 18:23:10 +0700

    Build Logic #3

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -7,6 +7,7 @@ import { Wrapper as PopperWrapper } from "~/components/Popper";
 import AccountItem from "~/components/AccountItem";
 import styles from "./Search.module.scss";
 import { SearchIcon } from "~/components/Icons";
+import { useDebounce } from "~/hooks";
 
 const cx = classNames.bind(styles);
 
@@ -16,17 +17,19 @@ function Search() {
   const [showResult, setShowResult] = useState(true);
   const [loading, setLoading] = useState(false);
 
+  const debounced = useDebounce(searchValue, 500);
+
   const inputRef = useRef();
 
   useEffect(() => {
-    if (!searchValue.trim()) {
+    if (!debounced.trim()) {
       setSearchResult([]);
       return;
     }
     setLoading(true);
     fetch(
       `https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(
-        searchValue,
+        debounced,
       )}&type=less`,
     )
       .then((response) => response.json())
@@ -37,7 +40,7 @@ function Search() {
       .catch(() => {
         setLoading(false);
       });
-  }, [searchValue]);
+  }, [debounced]);
 
   const handleClear = () => {
     setSearchValue("");
